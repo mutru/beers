9c0512acd38ee79575ff5bd12a0dc9aeef86687d
Otto Hilska <otto@flowdock.com>
2012-09-21 12:56:37 +0300

    Support for beer cases.

--- beers.rb
+++ beers.rb
@@ -3,6 +3,7 @@
 module Beer
   EMOJI = "🍺 "
   DOUBLE_EMOJI = "🍻 "
+  ERROR = "🚨  🚔  🚔  🚔  🚨 "
 end
 
 class Fixnum
@@ -14,12 +15,48 @@ class Fixnum
     Beer::DOUBLE_EMOJI * self
   end
 
+  def pack
+    if self == 6
+      print_pack(2, 3)
+    elsif self == 12
+      print_pack(2, 6)
+    elsif self == 24
+      print_pack(4, 6)
+    else
+      raise BeerError.new
+    end
+  end
+
   alias beer beers
+
+private
+
+  def print_pack(rows, cols)
+    (cols.beers + "\n") * rows
+  end
+end
+
+class BeerError < RuntimeError
+  def self.class
+    Class.new do
+      def to_s
+        Beer::ERROR
+      end
+    end
+  end
+
+  def to_s
+    Beer::ERROR
+  end
 end
 
 class String
   def compact
     (length / 4).double_beers + ((length % 4) / 2).beers
+  end
+
+  def inspect
+    "\n" + to_s
   end
 end
 
